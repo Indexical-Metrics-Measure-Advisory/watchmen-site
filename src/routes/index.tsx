d5764fb794d06dca067fbbf54430f81953bd70b5
botlikes <bradwoo8621@yandex.com>
2021-08-23 21:06:26 +0800

    init

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
-import {Suspense} from 'react';
-import {HashRouter, Redirect, Route, Switch} from 'react-router-dom';
+import {Suspense, useEffect} from 'react';
+import {HashRouter, Redirect, Route, Switch, useLocation} from 'react-router-dom';
 import {Router} from './router';
 import {Header} from '../header';
 import {Home} from '../home';
@@ -10,22 +10,36 @@ import {GetAQuote} from '../get-a-quote';
 import {Clients} from '../clients';
 import {PricingAndPlan} from '../pricing-plan';
 
+export const RoutesDelegate = () => {
+	const location = useLocation();
+
+	useEffect(() => {
+		if (window.scrollY !== 0) {
+			window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+		}
+	}, [location]);
+
+	return <>
+		<Header/>
+		<Switch>
+			<Route path={Router.HOME}><Home/></Route>
+			<Route path={Router.SERVICES}><Services/></Route>
+			<Route path={Router.GET_A_QUOTE}><GetAQuote/></Route>
+			<Route path={Router.PLAN_AND_PRICING}><PricingAndPlan/></Route>
+			<Route path={Router.CLIENTS}><Clients/></Route>
+			<Route path={Router.ABOUT}><About/></Route>
+			<Route path="*">
+				<Redirect to={Router.HOME}/>
+			</Route>
+		</Switch>
+		<Footer/>
+	</>;
+};
+
 export const Routes = () => {
 	return <Suspense fallback={<div/>}>
 		<HashRouter>
-			<Header/>
-			<Switch>
-				<Route path={Router.HOME}><Home/></Route>
-				<Route path={Router.SERVICES}><Services/></Route>
-				<Route path={Router.GET_A_QUOTE}><GetAQuote/></Route>
-				<Route path={Router.PLAN_AND_PRICING}><PricingAndPlan/></Route>
-				<Route path={Router.CLIENTS}><Clients/></Route>
-				<Route path={Router.ABOUT}><About/></Route>
-				<Route path="*">
-					<Redirect to={Router.HOME}/>
-				</Route>
-			</Switch>
-			<Footer/>
+			<RoutesDelegate/>
 		</HashRouter>
 	</Suspense>;
 };
